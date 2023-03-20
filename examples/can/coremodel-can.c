bf661c33f89a2b804a234d0b9f89628ce2be82fd
Stan Skowronek <stan@corellium.com>
2023-03-20 02:11:32 -0400

    Slightly cooler test that tests CAN host->VM path.

--- examples/can/coremodel-can.c
+++ examples/can/coremodel-can.c
@@ -6,6 +6,8 @@
 
 static const unsigned can_datalen[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 16, 20, 24, 32, 48, 64 };
 
+static void *handle = NULL;
+
 static int test_can_tx(void *priv, uint64_t ctrl, uint8_t *data)
 {
     unsigned dlen = can_datalen[(ctrl & CAN_CTRL_DLC_MASK) >> CAN_CTRL_DLC_SHIFT], idx;
@@ -19,11 +21,15 @@ static int test_can_tx(void *priv, uint64_t ctrl, uint8_t *data)
         printf("[%010lx]\n", ctrl);
     fflush(stdout);
 
+    if(coremodel_can_rx(handle, 0x8ac7ffff00, NULL))
+        fprintf(stderr, "Rx send failed\n");
+
     return CAN_ACK;
 }
 
 static void test_can_rxcomplete(void *priv, int nak)
 {
+    printf(" -> %d\n", nak);
 }
 
 static const coremodel_can_func_t test_can_func = {
@@ -33,7 +39,6 @@ static const coremodel_can_func_t test_can_func = {
 int main(int argc, char *argv[])
 {
     int res;
-    void *handle;
 
     if(argc != 3) {
         printf("usage: coremodel-can <address[:port]> <can>\n");
